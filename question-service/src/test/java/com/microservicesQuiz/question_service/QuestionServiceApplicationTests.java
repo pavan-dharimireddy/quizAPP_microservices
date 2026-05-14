e964fbbc5c64ffa7d9e34422b462e00c9630ad77
Pavan Dharimireddy <pavandharimireddy.999@gmail.com>
2026-05-14 16:19:34 +0530

    question-service added

---
+++ question-service/src/test/java/com/microservicesQuiz/question_service/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicesQuiz.question_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
